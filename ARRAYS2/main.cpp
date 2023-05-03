1f2a9b6b568ae85742ea7f54d765c8fc11e5f58b
Михаил Алексеев <Mihanik31313@hotmail.com>
2023-05-03 12:58:43 +0500

    Add project files.

---
+++ ARRAYS2/main.cpp
@@ -0,0 +1,42 @@
+// ARRAYS2
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int SIZE = 5;
+	int arr[SIZE] = { 3, 5, 8, 13, 21 };
+	//arr[2] = 123; //обращение к элементу массива на запись 
+	//cout << arr[2] << endl; // обращение к элементу массива на чтение 
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum << endl;
+	cout << "Среднее - арифметическое элементов массива: " << (double)sum / SIZE << endl;
+
+	int min, max;
+	min = max = arr[0];
+	for (int = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве: " << max << endl;
+}
---
+++ SHIFT/main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+//#define Shift left
+#define Shift right
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef Shift left
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	//Вывод исходного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	//Циклический сдвиг массива
+	int namber_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> namber_of_shifts;
+	for (int i = 0; i < namber_of_shifts; i++)
+	{
+		int boofer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = boofer;
+	}
+	//Вывод сдвинутого массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // Shift left
+
+	#ifdef	Shift right
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	//Вывод исходного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	//Циклический сдвиг массива вправо
+	int namber_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> namber_of_shifts;
+	for (int i = 0; i < namber_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+	}
+	//Вывод сдвинутого массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+}
+#endif // Shift right
+
+}
